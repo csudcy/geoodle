22b77a313ab330659bf1bc3c693cb1ae4e8a26dd
Nicholas Lee <csudcy@gmail.com>
2017-03-04 18:16:44 +0000

    Show a map

--- geoodle/app.py
+++ geoodle/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+import os
+
+from flask import Flask, request, jsonify, send_from_directory, render_template
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -6,7 +8,18 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-    return app.send_static_file('html/index.html')
+    # return app.send_static_file('html/index.html')
+    return render_template(
+        'index.html',
+        data={
+            'google_api_key': os.environ['GOOGLE_API_KEY']
+        }
+    )
+
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory(path)
 
 
 if __name__ == "__main__":
